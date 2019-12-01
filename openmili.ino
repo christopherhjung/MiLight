8b6b7c79920e640762079999be8f9bee56547928
Christopher Jung <christopher.h.jung@web.de>
2019-12-01 17:16:45 +0100

    ingo

--- openmili.ino
+++ openmili.ino
@@ -26,115 +26,15 @@ void setup()
 static int dupesPrinted = 0;
 static bool receiving = false;
 static bool escaped = false;
-static uint8_t outgoingPacket[7];
+static uint8_t outgoingPacket[7] = {0x07, 0xB0 , 0xF2 , 0xEA , 0x04 , 0x91 , 0x13 , 0xD3 , 0x44 , 0x99};
 static uint8_t outgoingPacketPos = 0;
 static uint8_t nibble;
-static enum {
-  IDLE,
-  HAVE_NIBBLE,
-  COMPLETE,
-} state;
+
 
 void loop()
 {
-  if (receiving) {
-    if (mlr.available()) {
-      printf("\n");
-      uint8_t packet[7];
-      size_t packet_length = sizeof(packet);
-      mlr.read(packet, packet_length);
-
-      for (int i = 0; i < packet_length; i++) {
-        printf("%02X ", packet[i]);
-      }
-    }
-
-    int dupesReceived = mlr.dupesReceived();
-    for (; dupesPrinted < dupesReceived; dupesPrinted++) {
-      printf(".");
-    }
-  }
-
-  while (Serial.available()) {
-    char inChar = (char)Serial.read();
-    uint8_t val = 0;
-    bool have_val = true;
-
-    if (inChar >= '0' && inChar <= '9') {
-      val = inChar - '0';
-    } else if (inChar >= 'a' && inChar <= 'f') {
-      val = inChar - 'a' + 10;
-    } else if (inChar >= 'A' && inChar <= 'F') {
-      val = inChar - 'A' + 10;
-    } else {
-      have_val = false;
-    }
-
-    if (!escaped) {
-      if (have_val) {
-        switch (state) {
-          case IDLE:
-            nibble = val;
-            state = HAVE_NIBBLE;
-            break;
-          case HAVE_NIBBLE:
-            if (outgoingPacketPos < sizeof(outgoingPacket)) {
-              outgoingPacket[outgoingPacketPos++] = (nibble << 4) | (val);
-            } else {
-              Serial.println("# Error: outgoing packet buffer full/packet too long");
-            }
-            if (outgoingPacketPos >= sizeof(outgoingPacket)) {
-              state = COMPLETE;
-            } else {
-              state = IDLE;
-            }
-            break;
-          case COMPLETE:
-            Serial.println("# Error: outgoing packet complete. Press enter to send.");
-            break;
-        }
-      } else {
-        switch (inChar) {
-          case ' ':
-          case '\n':
-          case '\r':
-          case '.':
-            if (state == COMPLETE) {
-              mlr.write(outgoingPacket, sizeof(outgoingPacket));
-            }
-            if(inChar != ' ') {
-              outgoingPacketPos = 0;
-              state = IDLE;
-            }
-            if (inChar == '.') {
-              mlr.resend();
-              delay(1);
-            }
-            break;
-          case 'x':
-            Serial.println("# Escaped to extended commands: r - Toggle receiver; Press enter to return to normal mode.");
-            escaped = true;
-            break;
-        }
-      }
-    } else {
-      switch (inChar) {
-        case '\n':
-        case '\r':
-          outgoingPacketPos = 0;
-          state = IDLE;
-          escaped = false;
-          break;
-        case 'r':
-          receiving = !receiving;
-          if (receiving) {
-            Serial.println("# Now receiving");
-          } else {
-            Serial.println("# Now not receiving");
-          }
-          break;
-      }
-    }
-  }
+    outgoingPacket[5] = 0;
+    outgoingPacket[5] = 255;
+    mlr.write(outgoingPacket, sizeof(outgoingPacket));
 }
 
